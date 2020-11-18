226c998344f2abe987659c654f2ecd1398420a28
Atul Jha <69789941+Atul240202@users.noreply.github.com>
2020-11-19 05:20:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = "/";
+const prefix = "#";
 client.on("message", function(message) {
 	if(message.author.bot) return;
 	if(!message.content.startsWith(prefix)) return;
 	const commandBody = message.content.slice(prefix.length);
 	const args = commandBody.split(' ');
 	const command = args.shift().toLowerCase();
-	if(command === "hello"){
+	if(command === "doubt"){
 	const timeTaken = Date.now() - message.createdTimestamp;
-	message.reply('Any doubt friend, if yes then click this');
+	message.reply('Fill this for (url), you will added to doubt solving channel when our experts will notice your question');
 
 }
 
