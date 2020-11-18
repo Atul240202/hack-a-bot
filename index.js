0b719101661ac77f176b7d9f9db874236c2168ea
Atul Jha <69789941+Atul240202@users.noreply.github.com>
2020-11-18 14:37:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = " ";
+const prefix = "/";
 client.on("message", function(message) {
 	if(message.author.bot) return;
 	if(!message.content.startsWith(prefix)) return;
 	const commandBody = message.content.slice(prefix.length);
 	const args = commandBody.split(' ');
 	const command = args.shift().toLowerCase();
-	if(command === "doubt"){
+	if(command === "hello"){
 	const timeTaken = Date.now() - message.createdTimestamp;
 	message.reply('Any doubt friend, if yes then click this');
 
