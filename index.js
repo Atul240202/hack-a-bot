501128ab0f908e99bc1ba1473486d1c56b700740
Atul Jha <69789941+Atul240202@users.noreply.github.com>
2020-11-19 11:15:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 let http = require('http');
-const prefix = "#";
-client.on("message", function(message) {
-	if(message.author.bot) return;
-	if(!message.content.startsWith(prefix)) return;
-	const commandBody = message.content.slice(prefix.length);
-	const args = commandBody.split(' ');
-	const command = args.shift().toLowerCase();
-	if(command === "doubt"){
-	const timeTaken = Date.now() - message.createdTimestamp;
-	message.reply('Fill this for (url), you will added to doubt solving channel when our experts will notice your question');
-	message.reply()
-
- 
     let handleRequest = (request, response) => {
         response.writeHead(200, {
             'Content-Type': 'text/plain'
@@ -27,5 +14,19 @@ client.on("message", function(message) {
     }
 
 });
+const prefix = "#";
+client.on("message", function(message) {
+	if(message.author.bot) return;
+	if(!message.content.startsWith(prefix)) return;
+	const commandBody = message.content.slice(prefix.length);
+	const args = commandBody.split(' ');
+	const command = args.shift().toLowerCase();
+	if(command === "doubt"){
+	const timeTaken = Date.now() - message.createdTimestamp;
+	message.reply('Fill this for (url), you will added to doubt solving channel when our experts will notice your question');
+	message.reply()
+
+ 
+
 client.login(process.env.token);
 
